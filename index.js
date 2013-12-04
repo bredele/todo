00ae2b13cddcb0c691e35f35b4772de498073c50
bredele <olivier.wietrich@gmail.com>
2013-12-04 00:51:39 -0700

    start refactoring

--- index.js
+++ index.js
@@ -11,50 +11,58 @@ var Each = require('each-plugin');
 var todo = new View();
 var list = new Store([]);
 var todos = new Each(list);
+
 stats = new Store({
-	completed: 0 //could be a ompute of length and left
-}); //i prefer Store({})
-stats.compute('left', function(){
-	return (list.data.length - this.completed).toString();
+	completed: 0
 });
 
-var length = 0; //trick meanwhile store push
+stats.compute('left', function(){
+	return list.data.length - this.completed; //to string?
+});
+
+
+
+
 
 //controller 
+
+function todoIndex(list, node){
+  var ul = [].slice.call(list.children);
+  return ul.indexOf(node.parentElement);
+}
+
+function completed(){
+  var l = list.data.length,
+     count = 0;
+	while(l--) {
+		//should may be be a boolean
+		if(list.get(l).status === 'completed') count++;
+	}
+	stats.set('completed', count);
+}
 
 var controller = {
 	//we should have an input plugin
 	submit: function(ev, node){
 		if(ev.keyCode === 13) {
 			//store should have push
-			list.set(length,{
+			list.set(list.data.length,{
 				label: node.value,
 				status: 'pending' //we set a class which is not needed
 			});
-			length++;
-			stats.set('left', length); //soso
 			node.value = "";
+			stats.set('left', stats.get('left') + 1); //better way?
 		}
 	},
 	//it seems really complicated
 	status: function(ev, node){
 		var target = ev.target;
-		var parent = target.parentElement;
-		var ul = [].slice.call(node.children); //use to array
-		var index = ul.indexOf(parent);
+		var index = todoIndex(node, target);
 		var store = todos.items[index].store;
 
-		//sheeeeeit
-		if(target.checked) {
-      store.set('status', 'completed');
-      stats.set('left', stats.get('left') - 1);
-      stats.set('completed', stats.get('completed') + 1);
-		} else {
-      store.set('status', 'pending'); 
-      stats.set('left', stats.get('left') + 1);
-      stats.set('completed', stats.get('completed') - 1);
-		}
-
+		//better if boolean
+		store.set('status', ev.target.checked ? 'completed' : 'pending');
+		completed();
 	},
 	toggleAll: function(){
 		//do store loop
@@ -65,6 +73,7 @@ var controller = {
 				status : 'completed'
 			});
 		}
+		stats.set('completed', l);
 	},
 	delAll : function(){
 		//do store loop
@@ -73,27 +82,20 @@ var controller = {
 			var item = list.get(l);
 			if(item.status === 'completed') list.del(l);
 		}
-		stats.set('completed', 0);
+		completed();
 	},
+
 	//the html attribute is huge :s
 	del: function(ev, node){
-		var ul = [].slice.call(node.children); //use to array
-		var index = ul.indexOf(ev.target.parentElement);
-		var store = todos.items[index].store;
-		if(store.get('status') === 'completed') {
-			stats.set('completed', stats.get('completed') - 1);
-		} else {
-			stats.set('left', stats.get('left') - 1);
-		}
-		length--;
-		list.del(index);
+		list.del(todoIndex(node, ev.target));
+		completed();
 	}
 };
 
 //bindings
 
 todo.html(document.getElementById('todoapp'), stats);
-todo.attr('events', new Events(controller));
 todo.attr('todos', todos);
+todo.attr('events', new Events(controller));
 todo.attr('visible', require('hidden-plugin'));
 todo.alive();
